7800858cefae212a787923588138c8ce3d4e420d
iamGauravMehta <84970875+iamGauravMehta@users.noreply.github.com>
2025-07-12 13:32:50 +0530

    Update start_container.sh
    changes for codedeploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull mehta0709/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 mehta0709/simple-python-flask-app
